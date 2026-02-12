9d2862cad72ebff8eb8dffd8cc30c90347f3131b
mahek073 <mahek.bscsf23@iba-suk.edu.pk>
2026-02-12 21:33:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// to scroll through the nav bars
 var navLinks = document.querySelectorAll('.nav-links a');
 
 for (var i = 0; i < navLinks.length; i++) {
@@ -13,12 +14,23 @@ for (var i = 0; i < navLinks.length; i++) {
     });
 }
 
+
+// to pop up the alert after message has been submitted and store it in the local storage
 var contactForm = document.querySelector('.contact-form');
 
-contactForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+if (contactForm) {
+    contactForm.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-    alert("Thank you for contacting The Quiet Cup Café! ☕ We’ll get back to you soon.");
-
-    contactForm.reset();
-});
+        var name = contactForm.querySelector('input[type="text"]').value;
+        var email = contactForm.querySelector('input[type="email"]').value;
+        var message = contactForm.querySelector('textarea').value;
+        
+        var submissions = JSON.parse(localStorage.getItem('submissions')) || [];
+        
+        submissions.push({ name: name, email: email, message: message });
+        localStorage.setItem('submissions', JSON.stringify(submissions));
+        alert("Thank you! Your message has been saved. ☕");
+        contactForm.reset();
+    });
+}
